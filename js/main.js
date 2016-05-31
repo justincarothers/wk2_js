974c38102ad1aa825f04377055b259419013475f
justincarothers <justincarothers@gmail.com>
2016-05-30 19:41:31 -0700

    IHTGIHRTG

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+
+
+  $(".background").animate({
+
+
+    marginLeft: "+=1000px"
+
+
+
+  }, 0);
+
+  $(".background").animate({
+
+
+    marginLeft: "-=1000px"
+
+
+
+  }, 500);
+
+
+
+
+
+  // $(".background").animate({
+  //
+  //
+  //   width: "+=200" ,
+  //   height: "toggle"
+  //
+  //
+  //  }, 5000);
+
+
+
+
+
+  // $("#moveUp").click(function(){
+  //   console.log("HI");
+  //   $("#moveUp").animate({
+  //     marginTop: "+=50px" ,
+  //     height: "toggle"
+  //
+  //   }, 1000);
+  // })
+
+
+
+
+
+
+
+
+})
